3d51e12188a70d080205debf88c28a09a518cb75
tanyaee <eremeeva.tallium@gmail.com>
2018-06-15 22:36:20 +0300

    added matrix transformation to column vector, informativity index is calculated correct now

--- informativity.py
+++ informativity.py
@@ -2,41 +2,42 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 import time
-#from scipy import ndimage
 
 '''program execution time'''
 start_time = time.clock()
 
-img = cv2.imread('image/industrial.jpg',0)
+original_image = cv2.imread('image/industrial.jpg', 0)
 surf = cv2.xfeatures2d.SURF_create(5000, extended=True,upright=True)
-kp, des = surf.detectAndCompute(img, None)
-img_kp = cv2.drawKeypoints(img,kp,None,(255,0,0),4)
-print('number of key points of img:', len(des))
+kp, des = surf.detectAndCompute(original_image, None)
+img_kp = cv2.drawKeypoints(original_image,kp,None,(255,0,0),4)
+print('number of key points of original image:', len(des))
 plt.imshow(img_kp),plt.show()
 print("descriptor size", surf.descriptorSize())
 
 '''image cropping'''
-I1 = img[0:400, 0:400]
+I1 = original_image[0:400, 0:400]
 
-#cv2.imshow("cropped", I1)#,plt.show()
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
 '''image to double representation'''
-img1 = cv2.normalize(I1.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
+image_1 = cv2.normalize(I1.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
+'''transform matrix to column vector '''
+A1 = image_1.flatten(order='F')
 
 '''covariance calculation'''
-c1 = np.cov(img1)
+image1_covariance = np.cov(A1)
 #print("covariance", c1)
 
 '''crop second image'''
-I2 = img[0:500, 0:500]
-img2 = cv2.normalize(I2.astype('float'),None,0.0,1.0, cv2.NORM_MINMAX)
-c2 = np.cov(img2)
-#c3 = c2/c1
-v2 = np.var(img2)
-print("variance img2: ", v2)
-'''SNR calculation'''
-snr2= np.mean(img2)/v2
+I2 = original_image[0:500, 0:500]
+image_2 = cv2.normalize(I2.astype('float'),None,0.0,1.0, cv2.NORM_MINMAX)
+A2 = image_2.flatten(order='F')
+
+image2_covariance = np.cov(A2)
+covariance = image1_covariance/image1_covariance
+image2_variance = np.var(image_2)
+print("variance of second image: ", image2_variance)
+
+'''signal to noise ratio calculation'''
+snr2= np.mean(image_2)/image2_variance
 print("snr", snr2)
 
 kp2, des2 = surf.detectAndCompute(I2, None)
@@ -44,6 +45,6 @@ kp2, des2 = surf.detectAndCompute(I2, None)
 print(time.clock() - start_time, "seconds")
 cv2.waitKey(0)
 
-'''Invormativity index'''
-H = v2 + snr2 + len(des)
+'''Invormativity index calculation'''
+H = image2_variance + snr2 + len(des)-covariance
 print("informativity index", H)
